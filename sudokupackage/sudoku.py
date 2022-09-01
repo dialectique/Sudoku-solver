521f6b21f3d8b32321da41b4d3c5a4d748573548
Gilles Chardon <gilles.chardon.fr@gmail.com>
2022-09-01 20:57:20 +0900

    create the package 'sudokupackage'

---
+++ sudokupackage/sudoku.py
@@ -0,0 +1,20 @@
+
+class Sudoku():
+    """
+    A class to solve sudoku
+    """
+
+
+    def ping(self):
+        """
+        You call ping I return pong.
+        """
+        return "PONG"
+
+
+def main():
+    print("The library sudoku.py has been ran directly.")
+
+
+if __name__ == "__main__":
+    main()
