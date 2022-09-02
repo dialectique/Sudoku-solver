2874ce9e3a8c168934e4f98e8994048013aee7ba
Gilles Chardon <gilles.chardon.fr@gmail.com>
2022-09-02 18:26:41 +0900

    create util.py and progress on sudoku.py: solver is fully functional

--- sudokupackage/sudoku.py
+++ sudokupackage/sudoku.py
@@ -1,8 +1,191 @@
+import csv
+import sys
+
+from sudokupackage.util import Node, StackFrontier, QueueFrontier
+
 
 class Sudoku():
     """
-    A class to solve sudoku
+    A class to represent a sudoku grid and solve it
     """
+
+    DIGITS_SET = set(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
+
+    def __init__(self, sudoku_grid: list = []) -> str:
+        """
+        convert a sudoku grid into a string
+        For example:
+        [[1, 2, 3],
+         [3, 4, 5],   --->   "123456789"
+         [6, 7, 8]]
+        :param sudoku_grid: nested list of integers, size 9*9,
+        0 <= integer <= 9 and 0 for empty cell.
+        :type sudoku_grid: list
+        :raise ValueError: if sudoku_grid is not a nested list of integers,
+        size 10*10, 0 <= integer <= 9 and 0 for empty cell.
+        :return: a string of integers, length = 81
+        :rtype: str
+        """
+
+        # if the grid doesn't have 9 rows
+        # or if each row doesn't have 9 columns
+        # or it each cell is not a integer
+        # or if each integer is not in range [1,9]
+        # raise ValueError
+        if not(len(sudoku_grid) == 9 and \
+            all([len(row) == 9 for row in sudoku_grid]) and \
+            all([isinstance(x, int) for x in sum(sudoku_grid, [])]) and \
+            all([0 <= x <= 9 for x in sum(sudoku_grid, [])])):
+            raise ValueError("sudoku grid must be a nested list, size 9*9, \
+                0 <= integer <= 9 and 0 for empty cell.")
+
+        # assign the converted grid into a string to this class attribute grid
+        self.grid = "".join(list(map(str, sum(sudoku_grid, []))))
+
+
+    def grid_from_string_to_list(self, grid):
+        """
+        convert a grid string into a nested list, size 9*9
+        """
+        return [[int(grid[j * 9 + i]) for i in range(9)] for j in range(9)]
+
+
+
+    def is_solved(self, grid: str) -> bool:
+        """
+        check if the sudoku grid is solved:
+        each column, each row, and each of the nine 3x3 subgrids
+        that compose the grid must contain all of the digits from 1 to 9
+        :param grid: sudoku grid converted into a string
+        :type: str
+        :return: True if the grid is solved, if not return False
+        :rtype: bool
+        """
+
+        # list of the columns
+        columns = [[grid[i+j] for i in range(0, 73, 9)] for j in range(9)]
+
+        # list of the rows
+        rows = [[grid[i+j] for i in range(9)] for j in range(0, 73, 9)]
+
+        # list of the subgrids
+        subgrids = [[grid[i+j+k] for i in range(3) for j in [0, 9, 18]]
+                    for k in [0, 3, 6, 27, 30, 33, 54, 57, 60]]
+
+        # check if each column, each row, and each subgrid contains
+        # all of the digits from 1 to 9
+        return all([
+            all([set(col) == Sudoku.DIGITS_SET for col in columns]),
+            all([set(row) == Sudoku.DIGITS_SET for row in rows]),
+            all([set(sub) == Sudoku.DIGITS_SET for sub in subgrids])
+        ])
+
+
+    def neighbors(self, state: tuple) -> list:
+        """
+        compute the neigbor grids,
+        ie compute the grid for each possible value at position
+        :param state: grid (string) and position (int)
+        :type: tuple
+        :return: a list of string where each string is a neighbor
+        :rtype: list
+        """
+        grid, position = state
+
+        # increment position while the cell is not empty (ie != 0)
+        while grid[position] != "0":
+            position +=1
+
+        # compute possible values in the row at position
+        row = set(grid[(position // 9) * 9 + i] for i in range(9))
+        possible_values_in_row = Sudoku.DIGITS_SET - row
+
+        # compute possible values in the column at position
+        column = set(grid[(position % 9) + i] for i in range(0, 73, 9))
+        possible_values_in_column = Sudoku.DIGITS_SET - column
+
+        # compute possible values in the subgrid at position
+        subgrid = set(grid[(position // 27) * 27 + \
+            ((position % 9) // 3) * 3 + i + j] \
+            for i in range(3) for j in [0, 9, 18])
+        possible_values_in_subgrid = Sudoku.DIGITS_SET - subgrid
+
+        # compute possible values at position
+        possible_values = possible_values_in_row & \
+            possible_values_in_column & \
+            possible_values_in_subgrid
+
+        # compute the grid for each possible value
+        # and return a list of the neighbors states, ie (grid, position)
+        grid_list = []
+        for val in possible_values:
+            # when position is 80, nothing to add after the possible value
+            if position == 80:
+                grid_list.append((
+                grid[:position] + val,
+                position
+                ))
+            else:
+                grid_list.append((
+                grid[:position] + val + grid[position+1:],
+                position
+                ))
+
+        return grid_list
+
+
+
+    def solve(self):
+        """
+        finds a solution to the sudoku grid, if one exists,
+        using breadth-first search
+        If several solutions exist, return the first finded
+        :return: nested list of integers, size 9*9,
+        1 <= integer <= 9, no 0 beacause no empty cell
+        if nosolution, return an empty list []
+        :rtype: list
+        """
+
+        # Keep track of number of states explored
+        self.num_explored = 0
+
+        # Initial state is the initial grid and position 0
+        # position will iterates from 0 to 80 as the grid
+        # have been convert into a string in __init__ method
+        self.initialisation = (self.grid, 0)
+
+        # Initialize frontier to just the starting position
+        start = Node(state=self.initialisation, parent=None)
+        frontier = StackFrontier()
+        frontier.add(start)
+
+        # Initialize an empty explored set
+        self.explored = set()
+
+        # Keep looping untill solution found
+        while True:
+
+            # If nothing left in frontier, then no solution
+            if frontier.empty():
+                return []
+
+            # Choose a node from the frontier
+            node = frontier.remove()
+            self.num_explored += 1
+
+            # if node is a solved sudoku grid, then return the solution
+            if self.is_solved(node.state[0]):
+                return self.grid_from_string_to_list(node.state[0])
+
+            # mark node as explored:
+            self.explored.add(node.state)
+
+            # Add neighbors to frontier
+            for neighbor_state in self.neighbors(node.state):
+                if not frontier.contains_state(neighbor_state) and \
+                    neighbor_state not in self.explored:
+                    child = Node(state=neighbor_state, parent=node)
+                    frontier.add(child)
 
 
     def ping(self):
@@ -13,7 +196,7 @@ class Sudoku():
 
 
 def main():
-    print("The library sudoku.py has been ran directly.")
+    print("The library essai.py has been ran directly.")
 
 
 if __name__ == "__main__":
--- tests/test_sudoku.py
+++ tests/test_sudoku.py
@@ -1,12 +1,198 @@
 """
-test sudoku.py from sudokupackage
+test sudokui.py from sudokupackage with pytest
 """
 
+import pytest
+
+# from sudokupackage.sudoku import Sudoku, main
 from sudokupackage.sudoku import Sudoku, main
 
-s = Sudoku()
+
+# list of invalid sudoku grids
+invalid_input = []
+
+# empty list
+invalid_input.append([])
+
+# invalid number of rows
+invalid_input.append([
+    [7,0,0,  1,0,0,  0,0,6],
+    [0,0,0,  6,0,0,  0,4,0],
+    [0,0,2,  0,0,8,  1,0,0],
+
+    [0,0,8,  0,0,0,  0,0,0],
+    [0,5,0,  8,0,6,  0,0,1],
+
+    [0,0,0,  0,0,0,  0,1,0],
+    [0,4,0,  5,0,0,  0,0,0],
+    [0,1,5,  0,0,7,  0,0,4]
+])
+
+# invalid number of colums
+invalid_input.append([
+    [7,0,0,  1,0,0,  0,0,6],
+    [0,0,0,  6,0,0,  0,4,0],
+    [0,0,2,  0,0,8,  1,0,0],
+
+    [0,8,  0,0,0,  0,0,0],
+    [0,5,0,  8,0,6,  0,0,1],
+    [1,0,0,  0,2,0,  0,0,0],
+
+    [0,0,0,  0,0,0,  0,1,0],
+    [0,4,0,  5,0,0,  0,0,0],
+    [0,1,5,  0,0,7,  0,0,4]
+])
+
+# invalid type
+invalid_input.append([
+    [7,0,0,  1,0,0,  0,0,6],
+    [0,0,0,  "6",0,0,  0,4,0],
+    [0,0,2,  0,0,8,  1,0,0],
+
+    [0,0,8,  0,0,0,  0,0,0],
+    [0,5,0,  8,0,6,  0,0,1],
+    [1,0,0,  0,2,0,  0,0,0],
+
+    [0,0,0,  0,0,0,  0,1,0],
+    [0,4,0,  5,0,0,  0,0,0],
+    [0,1,5,  0,0,7,  0,0,4]
+])
+
+# invalid range
+invalid_input.append([
+    [7,0,0,  1,0,0,  0,0,6],
+    [0,0,0,  6,0,0,  0,4,0],
+    [0,0,2,  0,0,11,  1,0,0],
+
+    [0,0,8,  0,0,0,  0,0,0],
+    [0,5,0,  8,0,6,  0,0,1],
+    [1,0,0,  0,2,0,  0,0,0],
+
+    [0,0,0,  0,0,0,  0,1,0],
+    [0,4,0,  -5,0,0,  0,0,0],
+    [0,1,5,  0,0,7,  0,0,4]
+])
+
+# valid sudoku grid
+valid_input = [
+    [5,3,0,  0,7,0,  0,0,0],
+    [6,0,0,  1,9,5,  0,0,0],
+    [0,9,8,  0,0,0,  0,6,0],
+
+    [8,0,0,  0,6,0,  0,0,3],
+    [4,0,0,  8,0,3,  0,0,1],
+    [7,0,0,  0,2,0,  0,0,6],
+
+    [0,6,0,  0,0,0,  2,8,0],
+    [0,0,0,  4,1,9,  0,0,5],
+    [0,0,0,  0,8,0,  0,7,9]
+]
+
+#solved_grid
+solved_grid = [
+    [5,3,4,  6,7,8,  9,1,2],
+    [6,7,2,  1,9,5,  3,4,8],
+    [1,9,8,  3,4,2,  5,6,7],
+
+    [8,5,9,  7,6,1,  4,2,3],
+    [4,2,6,  8,5,3,  7,9,1],
+    [7,1,3,  9,2,4,  8,5,6],
+
+    [9,6,1,  5,3,7,  2,8,4],
+    [2,8,7,  4,1,9,  6,3,5],
+    [3,4,5,  2,8,6,  1,7,9]
+]
+# unsolvable_grid
+valid_unsolvable_grid = [
+    [0,0,0,  4,5,6,  0,0,0],
+    [0,0,0,  1,2,3,  0,0,0],
+    [0,0,0,  8,9,7,  0,0,0],
+
+    [1,2,3,  0,0,0,  4,5,6],
+    [4,5,6,  0,0,0,  7,8,9],
+    [7,8,9,  0,0,0,  1,2,3],
+
+    [0,0,0,  7,8,9,  0,0,0],
+    [0,0,0,  2,3,1,  0,0,0],
+    [0,0,0,  9,8,7,  0,0,0]
+]
+
+# valid sudoku grid 2
+valid_input2 = [
+    [0,0,3,  0,8,4,  0,0,0],
+    [0,5,0,  2,0,0,  0,0,9],
+    [4,0,0,  0,0,1,  0,6,0],
+
+    [1,0,0,  8,0,0,  7,0,3],
+    [0,0,0,  0,0,0,  0,0,0],
+    [9,0,2,  0,0,6,  0,0,8],
+
+    [0,4,0,  7,0,0,  0,0,5],
+    [2,0,0,  0,0,9,  0,3,0],
+    [0,0,0,  1,3,0,  6,0,0]
+]
+
+#solved_grid 2
+solved_grid2 = [
+    [6,2,3,  9,8,4,  5,7,1],
+    [7,5,1,  2,6,3,  4,8,9],
+    [4,8,9,  5,7,1,  3,6,2],
+
+    [1,6,4,  8,9,2,  7,5,3],
+    [5,3,8,  4,1,7,  2,9,6],
+    [9,7,2,  3,5,6,  1,4,8],
+
+    [3,4,6,  7,2,8,  9,1,5],
+    [2,1,5,  6,4,9,  8,3,7],
+    [8,9,7,  1,3,5,  6,2,4]
+]
+
+
+
+def test_list_to_string_invalid_inputs():
+    for grid in invalid_input:
+        with pytest.raises(ValueError):
+            s = Sudoku(grid)
+
+def test_list_to_string_valid_input():
+    s = Sudoku(valid_input)
+    assert s.grid == "530070000600195000098000060800060003400" \
+        "803001700020006060000280000419005000080079"
+
+def test_is_solved_with_solved_grid():
+    s = Sudoku(solved_grid)
+    assert s.is_solved(s.grid) == True
+
+def test_is_solved_with_unsolved_grid():
+    s = Sudoku(valid_input)
+    assert s.is_solved(s.grid) == False
+
+def test_grid_from_string_to_list():
+    s = Sudoku(valid_input)
+    assert s.grid_from_string_to_list(s.grid) == valid_input
+
+def test_neighbors():
+    s = Sudoku(valid_input)
+    neighbor_states = s.neighbors((s.grid, 30))
+    grids = [n[0] for n in neighbor_states]
+    possible_values = [n[30] for n in grids]
+    assert set(possible_values) == set(["5", "7", "9"])
+
+def test_solve_solution_exists():
+    s = Sudoku(valid_input)
+    assert s.solve() == solved_grid
+
+def test_solve_solution_exists2():
+    s = Sudoku(valid_input2)
+    assert s.solve() == solved_grid2
+
+def test_solve_no_solution():
+    s = Sudoku(valid_unsolvable_grid)
+    assert s.solve() == []
+
 
 def test_ping():
+    s = Sudoku(valid_input)
     assert s.ping() == "PONG"
 
 def test_main():
